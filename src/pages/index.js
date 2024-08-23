1ff6f3b988fcd8f72b2dc7fa154b58822042f040
pratikjagtap4 <npratikjagtap2704@gmail.com>
2024-08-23 22:54:07 +0530

    routing pages

---
+++ src/pages/index.js
@@ -0,0 +1,17 @@
+import JobListing from "./JobListing";
+import JobPage from "./JobPage";
+import LandingPage from "./LandingPage";
+import MyJobs from "./MyJobs";
+import OnBoarding from "./OnBoarding";
+import PostJob from "./PostJob";
+import SavedJobs from "./SavedJobs";
+
+export {
+  JobListing,
+  JobPage,
+  LandingPage,
+  MyJobs,
+  OnBoarding,
+  PostJob,
+  SavedJobs,
+};
