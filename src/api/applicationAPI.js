0fe1b0b9da93ba0286e5dc8bf320c8dced861399
pratikjagtap4 <npratikjagtap2704@gmail.com>
2024-08-28 23:59:58 +0530

    jobs api

--- src/api/applicationAPI.js
+++ src/api/applicationAPI.js
@@ -6,18 +6,34 @@ export async function applyToJob(token, _, jobData) {
 
   const supabase = await supabaseClient(token);
 
-  const fileName = `resume-${jobData.candidate_id}-${jobData.name}`;
+  const fileName = `resume-${jobData.candidate_id}-${jobData.firstName}-${jobData.lastName}`;
 
-  const { error: storageError } = await supabase.storage
-    .from("resumes")
-    .upload(fileName, jobData.resume);
+  async function checkIfResumeExists(name) {
+    const { data, error } = await supabase.storage.from("resumes").list("", {
+      search: name,
+    });
 
-  if (storageError) {
-    console.log("error uploading resume", storageError);
-    return null;
+    if (error) {
+      console.error("Error fetching files:", error);
+      return false;
+    }
+
+    return data.some((file) => file.name === name);
   }
 
-  const resume = `${config.supabaseUrl}/storage/v1/object/public/resumes/filename`;
+  const fileExists = await checkIfResumeExists(fileName);
+  if (!fileExists) {
+    const { error: storageError } = await supabase.storage
+      .from("resumes")
+      .upload(fileName, jobData.resume);
+
+    if (storageError) {
+      console.log("error uploading resume", storageError);
+      return null;
+    }
+  }
+
+  const resume = `${config.supabaseUrl}/storage/v1/object/public/resumes/${fileName}`;
 
   const { data, error } = await supabase
     .from("application")
@@ -27,5 +43,28 @@ export async function applyToJob(token, _, jobData) {
   if (error) {
     console.log("Error submitting application", error);
   }
+  return data;
+}
+
+export async function updateApplicationsStatus(
+  token,
+  { job_id, candidate_id },
+  status
+) {
+  const supabase = await supabaseClient(token);
+
+  const { data, error } = await supabase
+    .from("application")
+    .update({ status })
+    .eq("job_id", job_id)
+    .eq("candidate_id", candidate_id)
+    .select();
+
+  if (error || data.length === 0) {
+    console.log("Error updating Applications status", error);
+    return null;
+  }
+  console.log(data);
+
   return data;
 }
